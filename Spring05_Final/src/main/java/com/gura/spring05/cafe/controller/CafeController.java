86859d502f067ab475e6b160a4f28f6a7ef560fd
jeageun <htpy123@naver.com>
2020-08-14 12:51:06 +0900

    Spring05 카페글 목록보기 구현중...

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/controller/CafeController.java
@@ -1,7 +1,11 @@
 package com.gura.spring05.cafe.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.gura.spring05.cafe.service.CafeService;
 
@@ -9,4 +13,12 @@ import com.gura.spring05.cafe.service.CafeService;
 public class CafeController {
 	@Autowired
 	private CafeService cafeServcie;
+	
+	@RequestMapping("/cafe/list")
+	public ModelAndView getList(HttpServletRequest request,
+			ModelAndView mView){
+				
+		mView.setViewName("cafe/list");
+		return mView;
+	}
 }
